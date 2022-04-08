6cc5ead358040ac3820f9768923469aa1bd70efb
josephsamuel02 <mrsamd02@gmail.com>
2022-04-08 11:03:48 +0100

    .

--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose,} from 'redux'
-import promiseMiddleWare from 'redux-promise'
-import rootReducer from './Store/Reducers/index';
-const composeenhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const createStorWithMiddleWare = composeenhancer(applyMiddleware(promiseMiddleWare))(createStore)
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store = {createStorWithMiddleWare(rootReducer)}>  
-      <App />
-     </Provider> 
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals811`
+// import { Provider } from "react-redux";
+// import { createStore, applyMiddleware, compose } from "redux";
+// import promiseMiddleWare from "redux-promise";
+// import rootReducer from "./Store/Reducers/index";
+// const composeenhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const createStorWithMiddleWare = composeenhancer(
+//     applyMiddleware(promiseMiddleWare)
+// )(createStore);
+
+// ReactDOM.render(
+//     <React.StrictMode>
+//         <Provider store={createStorWithMiddleWare(rootReducer)}>
+//             <App />
+//         </Provider>
+//     </React.StrictMode>,
+//     document.getElementById("root")
+// );
